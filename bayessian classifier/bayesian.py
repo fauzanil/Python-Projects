ee1421941ee777178e203831da0b84a96bfa0e00
fauzanil <fauzanil@live.com>
2016-11-24 11:57:02 +0700

    Menu repaired

--- bayessian classifier/bayesian.py
+++ bayessian classifier/bayesian.py
@@ -3,8 +3,8 @@ from loadcsv import *
 print("========Bayessian classifier========")
 #print("========github.com/fauzanil=========")
 filename = raw_input("Enter CSV File Name :")
-length = int(raw_input("Enter CSV Length :"))
-tupleraw = raw_input("Enter Tuple Test :")
+length = int(raw_input("Enter CSV Column Length :"))
+tupleraw = raw_input("Enter Tuple Test (With space without quote) :")
 tuple = list(map(str, tupleraw.split()))
 classified = classFinder(classProbability(dataLister(length,filename)[0],tuple))
-print "Tuple classified as :" + classified
+print "Tuple classified as : " + classified
