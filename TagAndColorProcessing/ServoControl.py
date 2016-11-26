c1da728df2fb2bd2325242a3b4a6b27c78209ed5
Fauzanil <fauzanil@live.com>
2016-11-26 17:43:29 +0700

    New servo, control

---
+++ TagAndColorProcessing/ServoControl.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+rotateBody = 2
+claw = 4
+upDown = 14
+forwardBackward = 3
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(rotateBody,GPIO.OUT)
+GPIO.setup(claw,GPIO.OUT)
+GPIO.setup(upDown,GPIO.OUT)
+GPIO.setup(forwardBackward,GPIO.OUT)
+
+rotateBodyp = GPIO.PWM(rotateBody,50)
+clawp = GPIO.PWM(claw,50)
+upDownp = GPIO.PWM(upDown,50)
+forwardBackwardp = GPIO.PWM(forwardBackward,50)
+
+
+rotateBodyp.start(5)
